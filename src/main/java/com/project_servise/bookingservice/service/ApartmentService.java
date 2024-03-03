0c5c4f75a25fb59e2651e095bbc0f7b03a8eb41c
Eusebiu.Jacot <sebi1.77@mail.ru>
2024-03-03 21:47:38 +0100

    ApartmentCategoryController

--- src/main/java/com/project_servise/bookingservice/service/ApartmentService.java
+++ src/main/java/com/project_servise/bookingservice/service/ApartmentService.java
@@ -2,7 +2,6 @@ package com.project_servise.bookingservice.service;
 
 import com.project_servise.bookingservice.dto.ApartmentDTO;
 import com.project_servise.bookingservice.dto.CreateApartmentDTO;
-import com.project_servise.bookingservice.entity.Apartment;
 
 import java.util.List;
 
