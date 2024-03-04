9d7f034e85ac9dbc0f128d4294f8cb2ca9c26382
GreamcAtch <123013184+GreamcAtch@users.noreply.github.com>
2024-03-05 00:35:19 +0100

    Finish PriceCategoryDto

---
+++ src/main/java/com/project_service/bookingservice/controller/PriceCategoryController.java
@@ -0,0 +1,26 @@
+package com.project_service.bookingservice.controller;
+
+import com.project_service.bookingservice.dto.PriceCategoryDto;
+import com.project_service.bookingservice.service.PriceCategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(path = "/price category")
+public class PriceCategoryController {
+    private final PriceCategoryService priceCategoryService;
+
+    @PostMapping("/new")
+    @ResponseStatus(HttpStatus.CREATED)
+    public PriceCategoryDto createPriceOfCategory(@RequestBody PriceCategoryDto priceCategoryDto){
+        return priceCategoryService.createPriceOfCategory(priceCategoryDto);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public PriceCategoryDto findById(@PathVariable("id") String id){
+        return priceCategoryService.findById(id);
+    }
+}
